f72f4c16ec7691eeb11d6d646be40bd69f78c462
Alexey Melezhik <melezhik@gmail.com>
2016-04-20 10:51:03 +0300

    use id command instead of greping /etc/group file

--- modules/ssh-and-check-sudo/story.pm
+++ modules/ssh-and-check-sudo/story.pm
@@ -1,17 +1,9 @@
 my $ssh_command = config()->{main}->{ssh_command};
-my $sudo_group = config()->{main}->{sudo_group};
-
 my $server = story_var('server');
+my $user = story_var('user');
 
-set_stdout('ssh_begin');
-
-my $command = "$ssh_command $server grep $sudo_group /etc/group 2>\&1; echo status: \$?";
+my $command = "$ssh_command $server id $user -Gn 2>\&1";
 
 set_stdout($command);
-
-for my $l (split /\n/, `$command`){
-  set_stdout($l);
-}
-
-set_stdout('ssh_end');
+set_stdout(`$command`);
 
